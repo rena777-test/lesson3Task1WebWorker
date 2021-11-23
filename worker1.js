f267264fc69a129433a9ae5f829bf6c9bc97e44e
Irina.Zelinko <zelirina23@gmail.com>
2021-11-23 10:11:26 +0300

    Renamed.

--- worker1.js
+++
@@ -1,6 +0,0 @@
-onmessage = function (e){
-    console.log('Получено сообщение от основного потока');
-    let workerResult = "Результат сложения : " + (parseInt(e.data[0] ,10) + parseInt(e.data[1],10));
-    console.log('Отправка сообщения в  основной поток');
-    this.postMessage(workerResult);
-}
